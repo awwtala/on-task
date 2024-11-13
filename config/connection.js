6c64a4538d09eee3a2bb3cfe7af71a20b1e0a910
Kun Wansom <wansom.kun@gmail.com>
2024-11-12 21:23:23 -0800

    file structuring

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// Connection to the database
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = process.env.DB.URL
+? new Sequelize(process.env.DB_URL)
+: new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect:'postgres',
+    }
+);
+
+module.exports = sequelize; 
