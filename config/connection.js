692e794999b31ec63fb8bc1d40c16730b5da6e39
Tala Awwad <awwadtala08@gmail.com>
2024-11-14 19:37:23 -0800

    data model

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 // Connection to the database
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const sequelize = process.env.DB.URL
-? new Sequelize(process.env.DB_URL)
-: new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect:'postgres',
-    }
-);
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+      }
+    );
 
-module.exports = sequelize; 
+module.exports = sequelize;
+
+//DONE
